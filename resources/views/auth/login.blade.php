d685417397003291b8bc198d87bbced411d2eeb6
Paulo Macario <paulo.macario@toxicologiapardini.com.br>
2024-05-31 16:26:56 -0300

    ajuste login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,13 +50,20 @@
                                 </div>
                             </div>
                         </div>
+                        <div class="row mb-3">
+                            <label for="" class="col-md-4 col-form-label text-md-end">&nbsp;</label>
+                            <div class="col-md-6">
+                                <div class="d-grid gap-2">
+                                    <button type="submit" class="btn btn-secondary btn-block">
+                                        {{ __('Login') }}
+                                    </button>                                    
+                                  </div>
+                            </div>
+                        </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
+                        <div class="row mb-3">
+                            <label for="" class="col-md-4 col-form-label text-md-end">&nbsp;</label>
+                            <div class="col-md-6">
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{-- {{ __('Forgot Your Password?') }} --}}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,84 +10,90 @@
     <title>Hello, world!</title>
   </head>
   <body>
-    <header id="menu">
-        <div class="pofile">
-            <img src="./img/profile.jpg" id="profile-img" alt="">
-            <h1 id="profile-name">Paulo Macario</h1>
-            <div class="social mt-3 text-center">
-                <a href="#"><i class="fa-brands fa-linkedin"></i></a>
-                <a href="#"><i class="fa-brands fa-github"></i></a>
-                <a href="#"><i class="fa-regular fa-envelope"></i></a>
-            </div>
+    <div class="row"  id="front">
+        <div class="col-xs-12 col-md-4 col-lg-3">
+            <header id="menu">
+                <div class="pofile">
+                    <img src="./img/profile.jpg" id="profile-img" alt="">
+                    <h1 id="profile-name">Paulo Macario</h1>
+                    <div class="social mt-3 text-center">
+                        <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                        <a href="#"><i class="fa-brands fa-github"></i></a>
+                        <a href="#"><i class="fa-regular fa-envelope"></i></a>
+                    </div>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="" class="link-menu link-menu d-flex">
+                                <div class="icon-menu">
+                                    <i class="fa-solid fa-user"></i>
+                                </div>
+                                <div>
+                                    Sobre
+                                </div>
+                            </a>
+                            </li>
+                        <li>
+                            <a href=""  class="link-menu link-menu d-flex">
+                                  <div class="icon-menu">
+                                    <i class="fa-solid fa-briefcase"></i>
+                                </div>
+                                <div>
+                                    Esperiências
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""  class="link-menu d-flex">
+                                <div class="icon-menu">
+                                    <i class="fa-solid fa-computer"></i>
+                                </div>
+                                <div>
+                                    Habilidades
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href=""  class="link-menu link-menu d-flex">
+                                  <div class="icon-menu">
+                                    <i class="fa-regular fa-envelope"></i>
+                                </div>
+                                <div>
+                                    Contato
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('login') }}" class="link-menu link-menu d-flex">
+                                <div class="icon-menu">
+                                    <i class="fa-regular fa-window-restore"></i>
+                                </div>
+                                <div>
+                                    Aplicações
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <footer id="footer">
+                <p class="text-center">© Copyright PauloMacari</p>
+                <p class="text-center">Designed by PauloMacario</p>
+            </footer>
         </div>
-        <nav>
-            <ul>
-                <li>
-                    <a href="" class="link-menu link-menu d-flex">
-                        <div class="icon-menu">
-                            <i class="fa-solid fa-user"></i>
-                        </div>
-                        <div>
-                            Sobre
-                        </div>
-                    </a>
-                    </li>
-                <li>
-                    <a href=""  class="link-menu link-menu d-flex">
-                          <div class="icon-menu">
-                            <i class="fa-solid fa-briefcase"></i>
-                        </div>
-                        <div>
-                            Esperiências
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href=""  class="link-menu d-flex">
-                        <div class="icon-menu">
-                            <i class="fa-solid fa-computer"></i>
-                        </div>
-                        <div>
-                            Habilidades
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href=""  class="link-menu link-menu d-flex">
-                          <div class="icon-menu">
-                            <i class="fa-regular fa-envelope"></i>
-                        </div>
-                        <div>
-                            Contato
-                        </div>
-                    </a>
-                </li>
-                <li>
-                    <a href="{{ route('login') }}" class="link-menu link-menu d-flex">
-                        <div class="icon-menu">
-                            <i class="fa-regular fa-window-restore"></i>
-                        </div>
-                        <div>
-                            Aplicações
-                        </div>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <section id="front">
-        <div class="message d-flex align-items-center justify-content-center">
-            <h1 class="text-center">Site em construção... </h1>
+        <div class="col-xs-12 col-md-8 col-lg-9">
+            <section >
+                <div class="message d-flex align-items-center justify-content-center">
+                    <h1 class="text-center">Site em construção... </h1>
+                </div>
+            </section>
         </div>
-    </section>
+    </div>
     {{-- <main>
         main
     </main> --}}
 
-    <footer id="footer">
-        <p class="text-center">© Copyright PauloMacari</p>
-        <p class="text-center">Designed by PauloMacario</p>
-    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
