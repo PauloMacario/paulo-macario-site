3bd10a5041cddf81a7c38e96fd0cc42ef9dd8be2
Paulo.macario <paulo.macario@toxicologiapardini.com.br>
2024-08-07 10:46:14 -0300

    command atualizacao de datas de processamento e pagamento de creditacoes

--- app/Console/Commands/PaymentTypeUpdateUpcomingDates.php
+++ app/Console/Commands/PaymentTypeUpdateUpcomingDates.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\ControlFinance\PaymentType;
+use Illuminate\Support\Carbon;
 
 class PaymentTypeUpdateUpcomingDates extends Command
 {
@@ -25,6 +27,50 @@ class PaymentTypeUpdateUpcomingDates extends Command
      */
     public function handle()
     {
-        logger()->info('control-finance:payment-type-update-upcoming-dates');
+        logger()->info("INICIANDO->payment-type-update-upcoming-dates");
+
+        $paymentTypes = PaymentType::where('installment_enable', '=', 1)->get();
+
+        foreach ($paymentTypes as $payType) {
+
+            if (! $payType->next_processing) {
+                continue;
+            }
+
+            $nextProcessing = Carbon::createFromFormat('Y-m-d', $payType->next_processing);
+            $nextPayment = Carbon::createFromFormat('Y-m-d', $payType->next_payment);
+
+            if (! $this->mustUpdate($nextProcessing)) {
+                continue;
+            }
+                
+            $update = [];
+            $update['previous_processing'] = $nextProcessing->format('Y-m-d');
+            $update['processing_day'] = $nextProcessing->format('d');
+            $update['payment_day'] = $nextPayment->format('d');
+
+            $updateNextProcessing = $nextProcessing->addMonth()->format('Y-m-d');
+            $updateNextPayment = $nextPayment->addMonth()->format('Y-m-d');
+            
+            $update['next_processing'] = $updateNextProcessing;
+            $update['next_payment'] = $updateNextPayment;
+
+            $payType->update($update);
+
+            logger()->info("Datas de processamento atualizadas: {$payType->description}");
+        }
+    }
+
+    private function mustUpdate($nextProcessing)
+    {
+        $now = Carbon::now();
+
+        logger()->info("DATA/HORARIO: {$now->format('Y-m-d H:i:s')}");
+
+        if ($now >= $nextProcessing) {
+            return true;
+        }
+
+        return false;
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('tp:remove-folha-mestre')->dailyAt('00:00');
     }
 
     /**
